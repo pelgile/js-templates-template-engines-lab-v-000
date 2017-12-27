e198721f6f1d764949723c76eac50c7899ea1882
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-27 07:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function createPost() {
 }
 
 function postComment() {
-  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
   var commentText = document.getElementById("commentText").value;
   var commenterName = document.getElementById("commenter").value;
 
   var commentsSection = document.getElementById("comments");
-  commentsSection.innerHTML += commentsTemplate({ 'commenter': commenterName, 'comment': commentText });
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
